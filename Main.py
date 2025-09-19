9797540c42b5e4c025940ee1e67c2517c978de89
padmadivya58-beep <224351902+padmadivya58-beep@users.noreply.github.com>
2025-09-19 13:56:17 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+print("🤖 Welcome to AI Interview Coach!")
+
+print("Question: Tell me about yourself.")
+answer = input("Your Answer: ")
+
+print("\n--- Feedback ---")
+print("- Good attempt! But try to add more details, like your strengths and achievements.")
+print("- Suggested Answer: 'I am Divya, a B.Com graduate with strong interest in IT and problem solving. I have learned computer skills and I am eager to apply them in a global company.'")
